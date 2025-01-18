20db145f8ac88d752b59a0c1c5c523116db44cf3
Sep Duijkersloot <Sep.duijkersloott@gmail.com>
2025-01-18 21:54:52 +0100

    Hero Particles + Magnifyer start

--- src/Experience/Utils/Debug.js
+++ src/Experience/Utils/Debug.js
@@ -4,7 +4,7 @@ export default class Debug {
     constructor() {
         this.gui = new GUI()
         this.gui.width = '400px'
-        // this.gui.close()
-        this.gui.open()
+        this.gui.close()
+        // this.gui.open()
     }
 }
